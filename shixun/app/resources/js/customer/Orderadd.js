a4518fe69af9be92a1ebd4cca420093bbc5dbc83
LR784726 <13153911@qq.com>
2019-12-18 16:11:25 +0800

    123

---
+++ shixun/app/resources/js/customer/Orderadd.js
@@ -0,0 +1,35 @@
+// 时间不能为空
+$(function () {
+    $("#yes").bind('click', function (event) {
+        if ($("#time").val() == '') {
+            $("#message").html("时间不能为空");
+            event.preventDefault();
+        }
+        else{
+            $("#message").html("");
+        }
+    });
+});
+// 地点不能为空
+$(function () {
+    $("#yes").bind('click', function (event) {
+        if ($("#didian").val() == '') {
+            $("#message_1").html("地点不能为空");
+            event.preventDefault();
+        }else{
+            $("#message_1").html("");
+        }
+    });
+});
+// 概要不能为空
+$(function () {
+    $("#yes").bind('click', function (event) {
+        if ($("#gaiyao").val() == '') {
+            $("#message_2").html("概要不能为空");
+            event.preventDefault();
+        }else{
+            $("#message_2").html("");
+        }
+    });
+});
+$('#time').datetimepicker();
